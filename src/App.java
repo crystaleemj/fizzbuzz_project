83e99378dd42e9ef6253cc1774e1b9fa710fce94
crystaleemj <ceemj_@hotmail.com>
2023-05-15 21:41:13 +0800

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Print integers one-to-100, but print “Fizz” 
+        // if an integer is divisible by three, “Buzz” 
+        // if an integer is divisible by five, and “FizzBuzz” 
+        // if an integer is divisible by both three and five.
+
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+
+    }
+}
